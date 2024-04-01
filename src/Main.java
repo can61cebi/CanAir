a69801efccf03c1e7449f83e8e533f5fffabc8a8
Can Cebi <can61cebi@gmail.com>
2024-04-02 01:31:59 +0300

    düzenleme: kullanıcı yazdırma eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import java.awt.Toolkit;
 import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.BorderLayout;
 
 public class Main {
@@ -11,26 +13,28 @@ public class Main {
         double width = screenSize.getWidth();
         double height = screenSize.getHeight();
 
-        // Boyut ayarlama
         int frameWidth = (int) (width * 0.57);
         int frameHeight = (int) (height * 0.70);
         int panelHeight = (int) (frameHeight * 0.11);
 
-        // Ana pencere
         JFrame frame = new JFrame("CanAir");
         frame.setSize(frameWidth, frameHeight);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        // Üst panel
         JPanel topPanel = new JPanel();
         topPanel.setBackground(new Color(35, 43, 56));
         topPanel.setPreferredSize(new Dimension(frameWidth, panelHeight));
 
+        String userName = DatabaseFetch.fetchUserNameById(2);
+        JLabel label = new JLabel("Merhaba, " + userName);
+        label.setFont(new Font("Arial", Font.BOLD, 24));
+        label.setForeground(Color.BLACK);
+        label.setBounds(10, 10, 400, 30);
+
         frame.add(topPanel, BorderLayout.NORTH);
+        frame.add(label);
 
         frame.setVisible(true);
     }
 }
-
-// Projede hatırlatmalar için yorumlar eklenmektedir.
